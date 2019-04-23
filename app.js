6cdd55b9473e314b78636d1c2eef867f9db4eb6c
Jay Paulson <paulsjv@gmail.com>
2019-04-22 21:45:50 -0500

    have request passing in url encoded

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import request from 'request';
-// Set up the express app
+
 const app = express();
-// get all todos
+
+// https://expressjs.com/en/resources/middleware/cors.html
+// This is CORS-enabled for all origins!
+app.use(cors());
+
 app.get('/api', (req, res) => {
-  request('http://www.google.com', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body) // Print the google web page.
-     }
-  })
+//  console.log(req);
+  console.log(decodeURIComponent(req.query.encodedUrl));
+//  console.log(req.query.encodedUrl);
+//  request('http://www.google.com', function (error, response, body) {
+//    if (!error && response.statusCode == 200) {
+//        console.log(body) // Print the google web page.
+//     }
+//  })
   res.status(200).send({
     success: 'true',
     message: 'todos retrieved successfully',
