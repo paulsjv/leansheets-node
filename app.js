d9d7fa078fd0a88e927cfb1676f696c83f79e30c
Jay Paulson <paulsjv@gmail.com>
2019-04-23 07:35:38 -0500

    adding github example

--- app.js
+++ app.js
@@ -12,11 +12,24 @@ app.get('/api', (req, res) => {
 //  console.log(req);
   console.log(decodeURIComponent(req.query.encodedUrl));
 //  console.log(req.query.encodedUrl);
-//  request('http://www.google.com', function (error, response, body) {
-//    if (!error && response.statusCode == 200) {
-//        console.log(body) // Print the google web page.
-//     }
-//  })
+//  User-Agent: Awesome-Octocat-App
+  var options = {
+    url: 'https://api.github.com/users/paulsjv',
+    headers: {
+        'User-Agent':'Awesome-Octacat-App'
+    }
+  }
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body) // Print the google web page.
+     }
+    else {
+        console.log('There was an error');
+        console.log(error);
+        console.log(response);
+        console.log(body);
+    }
+  })
   res.status(200).send({
     success: 'true',
     message: 'todos retrieved successfully',
