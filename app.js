533800c81bb5044a315cc3201f1f849afcf8314e
Jay Paulson <paulsjv@gmail.com>
2019-04-22 20:30:22 -0500

    adding an external call

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import request from 'request';
 // Set up the express app
 const app = express();
 // get all todos
 app.get('/api', (req, res) => {
+  request('http://www.google.com', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body) // Print the google web page.
+     }
+  })
   res.status(200).send({
     success: 'true',
     message: 'todos retrieved successfully',
